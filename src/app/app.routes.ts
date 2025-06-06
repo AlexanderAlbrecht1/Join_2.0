562fc4755b676abd038058226f5b7af66df632c0
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-06 23:21:18 +0200

    start with navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './main/summary/summary.component';
+import { AddTaskComponent } from './main/add-task/add-task.component';
+import { BoardComponent } from './main/board/board.component';
+import { ContactsComponent } from './main/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'summary', component: SummaryComponent},
+  {path: 'addtask', component: AddTaskComponent},
+  {path: 'board', component: BoardComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: '', component: SummaryComponent},
+];
