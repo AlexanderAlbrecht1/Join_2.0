5431bc2c60ef539d3248d0c8c5dff4839987ae46
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-06 23:31:36 +0200

    routes and links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { SummaryComponent } from './main/summary/summary.component';
 import { AddTaskComponent } from './main/add-task/add-task.component';
 import { BoardComponent } from './main/board/board.component';
 import { ContactsComponent } from './main/contacts/contacts.component';
+import { PrivacyPolicyComponent } from './legal/privacyPolicy/privacy-policy/privacy-policy.component';
+import { ImprintComponent } from './legal/imprint/imprint/imprint.component';
 
 export const routes: Routes = [
   {path: 'summary', component: SummaryComponent},
@@ -10,4 +12,6 @@ export const routes: Routes = [
   {path: 'board', component: BoardComponent},
   {path: 'contacts', component: ContactsComponent},
   {path: '', component: SummaryComponent},
+  {path: 'privacypolicy', component: PrivacyPolicyComponent},
+  {path: 'imprint', component: ImprintComponent},
 ];
