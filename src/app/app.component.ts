5c599d28166af744d96a6cd90a79c0ce29274fd1
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-08 20:02:55 +0200

    start header style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./main/navbar/navbar.component";
+import { HeaderComponent } from './main/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
