d5fbca0622428690050dcf1bd908410fe0d94075
Florian Zunke <florianzunke@gmx.de>
2025-06-09 10:24:06 +0200

    add Summary HTML

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -2,10 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
+  standalone: true,
   imports: [],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
+
 export class SummaryComponent {
+
+  greeting = '';
+  initials = '';
+
+  constructor(){
+    this.greetings()
+  }
+
+
+  greetings() {
+    const currentHour = new Date().getHours();
+
+    console.log(currentHour);
+    if (currentHour >= 1 && currentHour <= 10) {
+      this.greeting = 'Good Morning,'
+    } else
+    if (currentHour >= 11 && currentHour <= 18) {
+      this.greeting = 'Good afternoon,'
+    } else {
+      this.greeting = 'Good evening,'
+    }
+  }
+
+
 
 }
