1feea7392390dea76ff4f875bd8fed7e06571250
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-09 22:56:20 +0200

    changed contact nad user class for monogram

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  loggedUser: User;
 
+  constructor() {
+    this.loggedUser = {
+      color : "#9747FF",
+      email:"peter@spider.net",
+      id:"1",
+      initials:"PP",
+      name:"Peter Parker",
+      phone:"+49 161 1312 582"
+    };
+  }
 }
