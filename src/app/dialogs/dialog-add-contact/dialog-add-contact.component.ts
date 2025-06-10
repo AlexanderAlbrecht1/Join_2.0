8da7f9980ae5afbd4dfd2ae737d04d246350f9a8
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-10 21:42:56 +0200

    add contact button

---
+++ src/app/dialogs/dialog-add-contact/dialog-add-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-contact',
+  imports: [],
+  templateUrl: './dialog-add-contact.component.html',
+  styleUrl: './dialog-add-contact.component.scss'
+})
+export class DialogAddContactComponent {
+
+}
--- src/app/main/contacts/contacts.component.ts
+++ src/app/main/contacts/contacts.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SupabaseService } from '../../services/supabase.service';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { DialogAddContactComponent } from '../../dialogs/dialog-add-contact/dialog-add-contact.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, DialogAddContactComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
 
   contacts: any[] = []; // Array to hold contacts
+
+   readonly dialog = inject(MatDialog);
 
   constructor(private supabaseService: SupabaseService) {
     this.loadContacts();
@@ -19,6 +30,10 @@ export class ContactsComponent {
   async loadContacts() {
    this.contacts = await this.supabaseService.getContacts();
    console.log('Contacts loaded:', this.contacts);
+  }
+
+  openDialogAddContact() {
+    this.dialog.open(DialogAddContactComponent);
   }
 
 }
